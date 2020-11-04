b0c06873d17e1a62e25cdabb76ae7d2840261a3a
dashrafbasha <72728974+dashrafbasha@users.noreply.github.com>
2020-11-04 18:26:28 +0300

    using difflib library to avoid typo from user and search for the closest word

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,23 @@
 import json
+from difflib import get_close_matches
+
 data = json.load(open("data.json"))
+
 def translate(w):
+    w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        # data.keys() is a method in get_close_matches return list of the closest keys in the dictionary
+
+        yes_or_no = input("Did you mean %s instead? Enter y if yes, or n if no: " % get_close_matches(w, data.keys())[0])
+        # %s make variable of (s) which equal the sentence after the next %
+        if yes_or_no == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yes_or_no == "n":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else :
         return "The word does not exist , please double check it."
 word = input("Enter a word: ")
