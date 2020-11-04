25e1edd773a066a0215888814465bb4c52639e36
dashrafbasha <72728974+dashrafbasha@users.noreply.github.com>
2020-11-04 19:17:00 +0300

    manipulate the output to avoid producing list if there is more than one meaning of the word or iterating through string using for loop

--- dictionary.py
+++ dictionary.py
@@ -11,14 +11,19 @@ def translate(w):
         # data.keys() is a method in get_close_matches return list of the closest keys in the dictionary
 
         yes_or_no = input("Did you mean %s instead? Enter y if yes, or n if no: " % get_close_matches(w, data.keys())[0])
-        # %s make variable of (s) which equal the sentence after the next %
+        # %s make variable of (s) which equal the sentence after the next % like formating the string
         if yes_or_no == "y":
             return data[get_close_matches(w, data.keys())[0]]
         elif yes_or_no == "n":
             return "The word doesn't exist. Please double check it."
         else:
             return "We didn't understand your entry."
-    else :
+    else:
         return "The word does not exist , please double check it."
 word = input("Enter a word: ")
-print (translate(word))
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
